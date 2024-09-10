95b30db769cbda0cc2bd3e30cbcccd394476b539
Dimeji-go <dimeji.go77@gmail.com>
2024-09-10 17:21:19 +0100

    extra config

--- lua/dimeji/plugins/which-key.lua
+++ lua/dimeji/plugins/which-key.lua
@@ -5,22 +5,5 @@ return {
   init = function()
     vim.o.timeout = true
     vim.o.timeoutlen = 500
-  local wk = require("which-key")
-  wk.register(mappings, opts)
-  
-  wk.register({
-    ["<leader>st"] = {
-      desc = "Start Liveserver",
-      cmd = ":LiveServerStart<CR>",
-    },
-  })
-  wk.register({
-    ["<leader>sp"] = {
-      desc = "Stop Liveserver",
-      cmd = ":LiveServerStop<CR>",
-    },
-  })
-
-
   end,
 }}
