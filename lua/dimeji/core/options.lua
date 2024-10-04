1e8b53434e208f4f00b3e165b0825f977f8f9d09
Dimeji-go <dimeji.go77@gmail.com>
2024-10-04 08:38:02 +0100

    fix clipboard

--- lua/dimeji/core/options.lua
+++ lua/dimeji/core/options.lua
@@ -47,17 +47,4 @@ opt.splitbelow = true -- split horizontal window to the bottom
 vim.o.swapfile = false
 
 
-if vim.fn.has("wsl") then
-    vim.g.clipboard = {
-        name = "clip.exe (Copy Only)",
-        copy = {
-            ["+"] = "clip.exe",
-            ["*"] = "clip.exe"
-        },
-        paste = {
-            ["+"] = "clip.exe",
-            ["*"] = "clip.exe"
-        },
-        cache_enabled = true
-    }
-end
+
