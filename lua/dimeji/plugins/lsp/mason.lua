537c19631e11b34604d555324acd87a85d317e45
Dimeji <dimeji-go77@gmail.com>
2025-06-04 11:48:13 +0100

    remove registry

--- lua/dimeji/plugins/lsp/mason.lua
+++ lua/dimeji/plugins/lsp/mason.lua
@@ -2,7 +2,6 @@ return {
   "williamboman/mason.nvim",
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
-    "mason-org/mason-registry",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
   config = function()
